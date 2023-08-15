e60054e618737f9a37e28ae6e3446e0a607e9d17
Thắng Lê <lequocthang307@gmail.com>
2023-08-15 09:57:05 +0700

    sghn

--- 01_co_so_du_lieu_quan_he/excercise/question1.sql
+++ 01_co_so_du_lieu_quan_he/excercise/question1.sql
@@ -1,20 +1,20 @@
 CREATE DATABASE `lesson_1`;
 USE `lesson_1`;
-CREATE TABLE `Classes`(
+CREATE TABLE `classes`(
 	id int primary key auto_increment,
     `name` varchar(200)
 );
-CREATE TABLE `Teachers`(
+CREATE TABLE `teachers`(
 	`id` int primary key auto_increment,
     `name` varchar(200),
     `age` int,
     `country` varchar(200)
 );
-INSERT INTO `Classes`(`name`)
+INSERT INTO `classes`(`name`)
 values 
 	("Lớp 1"),
 	("Lớp 2"),
 	("Lớp 3"),
 	("Lớp 4");
-INSERT INTO `Teachers`(`name`,`age`,`country`)
+INSERT INTO `teachers`(`name`,`age`,`country`)
 values ("Thắng",18,"Việt Nam")
